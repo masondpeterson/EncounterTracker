fd4011db7485ea88b833ffada5b085163d68894d
Mason Peterson <masondpeterson@gmail.com>
2020-02-04 21:05:16 -0700

    Created DB connection files and stubs for initial objects

---
+++ EncounterTracker/DataBase/Constants.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace EncounterTracker.DataBase
+{
+    public static class Constants
+    {
+        //Built from the example found in Microsoft Docs
+        //https://docs.microsoft.com/en-us/xamarin/xamarin-forms/data-cloud/data/databases
+
+        public const string DatabaseFilename = "sqlData.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            // open the database in read/write mode
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            // create the database if it doesn't exist
+            SQLite.SQLiteOpenFlags.Create |
+            // enable multi-threaded database access
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, DatabaseFilename);
+            }
+        }
+    }
+}
---
+++ EncounterTracker/DataBase/SQLConn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace EncounterTracker.DataBase
+{
+    public class SQLConn
+    {
+        private SQLiteConnection _conn;
+
+        public SQLConn()
+        {
+            _conn = new SQLiteConnection(Constants.DatabasePath);
+        }
+    }
+}
