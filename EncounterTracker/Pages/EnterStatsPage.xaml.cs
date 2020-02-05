3b45dbdd2858bbb36382400a9833bd8954ab8fb0
Mason Peterson <masondpeterson@gmail.com>
2020-02-04 20:39:56 -0700

    Added Page Classes

---
+++ EncounterTracker/Pages/EnterStatsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EncounterTracker
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EnterStatsPage : ContentPage
+    {
+        public EnterStatsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
