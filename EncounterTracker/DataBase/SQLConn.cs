7313e8e1abfc8b5f6699b4912cddb421925fd27e
Mason Peterson <masondpeterson@gmail.com>
2020-02-10 22:25:41 -0700

    Finished all but decryption in login

--- EncounterTracker/DataBase/SQLConn.cs
+++ EncounterTracker/DataBase/SQLConn.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using EncounterTracker.DBObjects;
 using SQLite;
 
 namespace EncounterTracker.DataBase
@@ -12,6 +13,36 @@ namespace EncounterTracker.DataBase
         public SQLConn()
         {
             _conn = new SQLiteConnection(Constants.DatabasePath);
+            _conn.CreateTable<User>();
         }
+
+        #region User Methods
+
+        public int InsertUser(User user)
+        {
+            var result = _conn.Insert(user);
+            return result;
+        }
+
+        public List<string> GetUserNames()
+        {
+            var usernames = new List<string>();
+            var users = _conn.Table<User>();
+            foreach (var user in users)
+            {
+                usernames.Add(user.Username);
+            }
+            return usernames;
+        }
+
+        public User GetUserByName(string username)
+        {
+            var user = from u in _conn.Table<User>()
+                       where u.Username == username
+                       select u;
+            return user.First();
+        }
+
+        #endregion
     }
 }
--- EncounterTracker/Pages/LoginPage.xaml.cs
+++ EncounterTracker/Pages/LoginPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EncounterTracker.DataBase;
+using EncounterTracker.DBObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,20 +14,128 @@ namespace EncounterTracker
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
+        private SQLConn _conn = new SQLConn();
+        private string encryptionPassword = "SD7389DKJ";
+        private List<string> Usernames;
+        private bool nameCheck;
+        private bool passCheck;
+
         public LoginPage()
         {
             Title = "Encounter Tracker";
             InitializeComponent();
+            title.Text = "    Welcome To\r\nEncounter Tracker";
         }
 
         private void regButton_Clicked(object sender, EventArgs e)
         {
-
+            //Alter page text to show registration
+            title.Text = "Create Account";
+            nameEntry.Placeholder = "Enter New Username";
+            passEntry.Placeholder = "Enter New Password";
+            lowerTitleLabel.IsVisible = false;
+            logButton.IsVisible = false;
+            regButton.IsVisible = false;
+            createButton.IsVisible = true;
+            nameEntry.TextChanged += createUsername_TextChanged;
+            passEntry.TextChanged += createPassword_TextChanged;
+            Usernames = _conn.GetUserNames();
         }
 
-        private void logButton_Clicked(object sender, EventArgs e)
+        async void logButton_Clicked(object sender, EventArgs e)
         {
-
+            var check = ValidateUser(nameEntry.Text, passEntry.Text);
+            if (check)
+            {
+                await Navigation.PushAsync(new HomePage(), true);
+            }
+            else
+            {
+                await DisplayAlert("Alert", "Username and Password are not valid.", "OK");
+            }
         }
+
+        async void createButton_Clicked(object sender, EventArgs e)
+        {
+            if (nameCheck && passCheck)
+            {
+                var user = new User();
+                var newPass = Crypto.Encrypt(passEntry.Text, encryptionPassword);
+                user.Username = nameEntry.Text;
+                user.Password = newPass;
+                _conn.InsertUser(user);
+                await Navigation.PushAsync(new LoginPage(), true);
+            }
+            else
+            {
+                await DisplayAlert("Alert", "Please Enter a Valid Username and Password", "OK");
+            }
+        }
+
+        private void createUsername_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var check = false;
+            foreach (var name in Usernames)
+            {
+                if (name == nameEntry.Text)
+                {
+                    check = true;
+                }
+            }
+            if (check)
+            {
+                nameValidate.TextColor = Color.Red;
+                nameValidate.Text = "Username Unavailable";
+                nameValidate.IsVisible = true;
+                nameCheck = false;
+            }
+            else
+            {
+                nameValidate.TextColor = Color.Green;
+                nameValidate.Text = "Username Available";
+                nameValidate.IsVisible = true;
+                nameCheck = true;
+            }
+        }
+
+        private void createPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (passEntry.Text.Length < 3)
+            {
+                passValidate.TextColor = Color.Red;
+                passValidate.Text = "Password must be more than 3 Characters";
+                passValidate.IsVisible = true;
+                passCheck = false;
+            }
+            else
+            {
+                passValidate.TextColor = Color.Green;
+                passValidate.Text = "Valid Password";
+                passValidate.IsVisible = true;
+                passCheck = true;
+            }
+        }
+
+        #region Support Methods
+
+        private bool ValidateUser(string username, string password)
+        {
+            var check = false;
+            //Get the user and validate the password matches the DB
+            var user = _conn.GetUserByName(username);
+            if (user != null)
+            {
+                var userpass = user.Password;
+                var pass = Crypto.Encrypt(password, encryptionPassword);
+                if (pass == password)
+                {
+                    check = true;
+                }
+            }
+
+            return check;
+        }
+
+        #endregion
     }
 }
