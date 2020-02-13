2bdb00e5cfd835d0833c88af4e84515b9fd0b5a0
Mason Peterson <masondpeterson@gmail.com>
2020-02-12 22:04:50 -0700

    Finished Encounter Search Functionality

--- EncounterTracker/Pages/HomePage.xaml.cs
+++ EncounterTracker/Pages/HomePage.xaml.cs
@@ -19,7 +19,6 @@ namespace EncounterTracker
         private int _userId;
         private SQLConn _conn = new SQLConn();
         private List<Character> Characters;
-        private List<Encounter> _eList;
         private int _selIndex = -1;
         private Character _selChar;
 
@@ -92,83 +91,19 @@ namespace EncounterTracker
 
         #endregion
 
-        #region Reporting Section Methods
+        #region Search Section Methods
 
-        async void searchButton_Clicked(object sender, EventArgs args)
+        async void searchButton_Clicked(object sender, EventArgs e)
         {
-            //pupulate the eList with ALL the players known encounters
-            _eList = _conn.GetEncountersByPlayer(_userId);
-            //Search the list for names that contain the search term
-            var elist = SearchEncounters(searchInput.Text);
-            if (elist.Count == 0)
-            {
-                await DisplayAlert("Alert", "No Matching Encounters were found \r\nso an unfiltered list was returned", "OK");
-                BuildResultFrames(_eList);
-            }
-            else
-            {
-                BuildResultFrames(elist);
-            }
+            await Navigation.PushAsync(new SearchResultsPage(searchInput.Text, _userId));
         }
 
-        private void refreshButton_Clicked(object sender, EventArgs e)
-        {
-            BuildResultFrames(_eList);
-        }
-
-        private List<Encounter> SearchEncounters(string searchTerm)
-        {
-            var elist = new List<Encounter>();
-            foreach (var e in _eList)
-            {
-                //check if whole search term is contained in the encounter name or if they start with the same letter. 
-                if (e.EncounterName.Contains(searchTerm))
-                {
-                    elist.Add(e);
-                }
-            }
-
-            return elist;
-        }
-
-        private void BuildResultFrames(List<Encounter> encounters)
-        {
-            foreach(var e in encounters)
-            {
-                var frame = new Frame
-                {
-                    BorderColor = Color.Gray,
-                    HasShadow = true,
-                    Content = new StackLayout
-                    {
-                        Orientation = StackOrientation.Horizontal,
-                        Children =
-                        {
-                            new Label
-                            {
-                                Text = e.EncounterName,
-                                HorizontalOptions = LayoutOptions.CenterAndExpand
-                                
-                            },
-                            new Label
-                            {
-                                Text = _conn.GetCharacterById(e.CharId).CharName,
-                                HorizontalOptions = LayoutOptions.CenterAndExpand
-                            },
-                            new Button
-                            {
-                                Text = "Stats",
-                                TabIndex = e.EncounterId,
-                                HorizontalOptions = LayoutOptions.CenterAndExpand
-                            }
-                        }
-                    }
-                };
-                resultsSection.Children.Add(frame);
-            }
-        }
         #endregion
 
+        #region Reports Section Methods
 
+
+
+        #endregion
     }
 }
---
+++ EncounterTracker/Pages/SearchResultsPage.xaml.cs
@@ -0,0 +1,153 @@
+﻿using EncounterTracker.DataBase;
+using EncounterTracker.DBObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EncounterTracker
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchResultsPage : ContentPage
+    {
+        private SQLConn _conn = new SQLConn();
+        private int _userId;
+        private List<Encounter> _elist;
+
+        public SearchResultsPage(string searchTerm, int userId)
+        {
+            _userId = userId;
+            Title = "Encounter Search Results";
+            InitializeComponent();
+
+            _elist = SearchEncounters(searchTerm, userId);
+            BuildResultFrames(_elist);
+        }
+
+        private List<Encounter> SearchEncounters(string searchTerm, int userId)
+        {            
+            var elist = _conn.GetEncountersByPlayer(userId);
+            var encounters = new List<Encounter>();
+            if (searchTerm == null || searchTerm == "")
+            {
+                return elist;
+            }
+            else
+            {
+                foreach (var e in elist)
+                {
+                    var name = e.EncounterName.ToLower();
+                    if (name.Contains(searchTerm.ToLower()))
+                    {
+                        encounters.Add(e);
+                    }
+                }
+            }
+            return encounters;
+        }
+
+        private void BuildResultFrames(List<Encounter> encounters)
+        {
+            if(encounters.Count == 0)
+            {
+                var label = new Label
+                {
+                    Text = "No Results Found",
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                    FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label))
+                };
+                resultsSection.Children.Add(label);
+            }
+            else
+            {
+                //Builds the Frames and Adds them to the page
+                foreach (var e in encounters)
+                {
+                    string dropped;
+                    if(e.Dropped == 1)
+                    {
+                        dropped = "yes";
+                    }
+                    else
+                    {
+                        dropped = "no";
+                    }
+                    var frame = new Frame
+                    {
+                        BorderColor = Color.Gray,
+                        HasShadow = true,
+                        Content = new StackLayout
+                        {
+                            Orientation = StackOrientation.Horizontal,
+                            Children =
+                        {
+                            new Label
+                            {
+                                Text = e.EncounterName,
+                                HorizontalOptions = LayoutOptions.CenterAndExpand
+
+                            },
+                            new Label
+                            {
+                                Text = _conn.GetCharacterById(e.CharId).CharName,
+                                HorizontalOptions = LayoutOptions.CenterAndExpand
+                            },
+                            new StackLayout
+                            {
+                                Children =
+                                {
+                                    new Label
+                                    {
+                                        Text = "Kills: " + e.Kills
+                                    },
+                                    new Label
+                                    {
+                                        Text = "Assists: " + e.Assist
+                                    },
+                                    new Label
+                                    {
+                                        Text = "Dropped: " + dropped
+                                    }
+                                }
+                            },
+                            new StackLayout
+                            {
+                                Children =
+                                {
+                                    new Label
+                                    {
+                                        Text = "Dealt: " + e.DmgDealt
+                                    },
+                                    new Label
+                                    {
+                                        Text = "Taken: " + e.DmgTaken
+                                    },
+                                    new Label
+                                    {
+                                        Text = "Healing: " + e.Healing
+                                    }
+                                }
+                            }
+                        }
+                        }
+                    };
+                    resultsSection.Children.Add(frame);
+                }
+            }
+        }
+
+        async void searchButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SearchResultsPage(searchInput.Text, _userId));
+        }
+
+        async void backButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HomePage(_userId));
+        }
+    }
+}
